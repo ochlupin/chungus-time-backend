d14824a405c2ef66f548763cd54c02e2592db953
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2019-01-15 12:42:36 -0500

    added default action definitions for users timers and projects cont

--- app/controllers/api/v1/timers_controller.rb
+++ app/controllers/api/v1/timers_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::TimersController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::UsersController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 end
