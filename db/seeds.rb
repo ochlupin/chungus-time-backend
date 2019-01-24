29d9323d057b9f5af12ce16471d1afba6ec4d41d
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2019-01-23 21:40:13 -0500

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(username: "phong", email: "phong@flatiron.com", password: "test")
+User.create!(username: "ali", email: "ali@flatiron.com", password: "test")
+User.create!(username: "yoan", email: "yoan@flatiron.com", password: "test")
+User.create!(username: "alex", email: "alex@flatiron.com", password: "test")
+
+Project.create!(title: "Frontend")
+Project.create!(title: "Backend")
+Project.create!(title: "Job Search")
+Project.create!(title: "Styling")
